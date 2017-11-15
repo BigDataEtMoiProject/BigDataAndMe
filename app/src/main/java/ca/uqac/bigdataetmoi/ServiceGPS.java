ab5a08e4e6be4fc4723dce09736ed01b2226b92a
Winnilis <agathe.guillemot@gmail.com>
2017-11-15 13:19:30 -0500

    preparing merge into master

--- app/src/main/java/ca/uqac/bigdataetmoi/ServiceGPS.java
+++ app/src/main/java/ca/uqac/bigdataetmoi/ServiceGPS.java
@@ -1,7 +1,5 @@
 package ca.uqac.bigdataetmoi;
 
-import android.*;
-import android.Manifest;
 import android.app.IntentService;
 import android.content.Context;
 import android.content.Intent;
@@ -13,7 +11,6 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.widget.Toast;
