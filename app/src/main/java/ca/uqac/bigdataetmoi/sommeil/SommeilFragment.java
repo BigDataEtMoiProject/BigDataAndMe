5035b66f4e7cd1d0a4ebb011b2585bdbbe829fa9
Patrick Lapointe <pingouin000@hotmail.com>
2018-05-17 19:43:19 -0400

    Faire calcul du temps de sommeil

--- app/src/main/java/ca/uqac/bigdataetmoi/sommeil/SommeilFragment.java
+++ app/src/main/java/ca/uqac/bigdataetmoi/sommeil/SommeilFragment.java
@@ -6,6 +6,9 @@ import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 import ca.uqac.bigdataetmoi.R;
 
@@ -13,11 +16,17 @@ public class SommeilFragment extends Fragment implements ISommeilContract.View {
 
     private ISommeilContract.Presenter presenter;
 
+    private TextView moisDernierTextView, semDernTextView, hierTextView;
+
     public SommeilFragment() { }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedBundle) {
         View rootView = inflater.inflate(R.layout.fragment_sommeil, container, false);
+
+        moisDernierTextView = rootView.findViewById(R.id.moisDernierTextView);
+        semDernTextView = rootView.findViewById(R.id.semDernTextView);
+        hierTextView = rootView.findViewById(R.id.hierTextView);
 
         return rootView;
     }
@@ -32,6 +41,24 @@ public class SommeilFragment extends Fragment implements ISommeilContract.View {
     @Override
     public void setPresenter(@NonNull ISommeilContract.Presenter presenter) {
         this.presenter = presenter;
+    }
+
+    @Override
+    public void afficherMoyenneMoisDernier(double moyenne) {
+        DecimalFormat df = new DecimalFormat("#.##");
+        moisDernierTextView.setText(df.format(moyenne) +  " h");
+    }
+
+    @Override
+    public void afficherMoyenneSemaineDern(double moyenne) {
+        DecimalFormat df = new DecimalFormat("#.##");
+        semDernTextView.setText(df.format(moyenne) + " h");
+    }
+
+    @Override
+    public void afficherTempsHier(double temps) {
+        DecimalFormat df = new DecimalFormat("#.##");
+        hierTextView.setText(df.format(temps) + " h");
     }
 
 }
