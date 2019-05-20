778034e1c2c7ff003b40b969afa842fae350bad2
Yvan <yvankiller92@gmail.com>
2019-05-20 14:40:53 +0200

    ajout de demande de permissions au lancement de l'application

--- app/src/main/java/ca/uqac/bigdataetmoi/startup/MainMenuActivity.java
+++ app/src/main/java/ca/uqac/bigdataetmoi/startup/MainMenuActivity.java
@@ -2,19 +2,18 @@ package ca.uqac.bigdataetmoi.startup;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.view.MenuItem;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -159,32 +158,16 @@ public class MainMenuActivity extends BaseActivity {
                 });
     }
 
-    class ViewPagerAdapter extends FragmentPagerAdapter {
-        private final List<Fragment> mFragmentList = new ArrayList<>();
-        private final List<String> mFragmentTitleList = new ArrayList<>();
 
-        public ViewPagerAdapter(FragmentManager manager) {
-            super(manager);
-        }
 
-        @Override
-        public Fragment getItem(int position) {
-            return mFragmentList.get(position);
-        }
-
-        @Override
-        public int getCount() {
-            return mFragmentList.size();
-        }
-
-        public void addFrag(Fragment fragment, String title) {
-            mFragmentList.add(fragment);
-            mFragmentTitleList.add(title);
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return mFragmentTitleList.get(position);
-        }
+    public void launchCommunicationActivity(View view) {
+        startActivity(new Intent(this, TelephoneSmsActivity.class));
     }
+
+
+
+    public void launchApplicationDetailsActivity(View view) {
+        startActivity(new Intent(this, UsageActivity.class));
+    }
+
 }
