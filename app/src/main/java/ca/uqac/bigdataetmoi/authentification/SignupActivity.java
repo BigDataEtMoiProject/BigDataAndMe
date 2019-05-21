004c1dc304608cd9b510fa5b6b4d5b23a5f603c6
Antoine <34485984+ohanqo@users.noreply.github.com>
2019-05-21 22:16:37 +0200

    Feature/register (#14)
    * feat(login): login from backend
    
    * feat(login): add user credentials
    
    * feat(login): login from backend
    
    * feat(login): add user credentials
    
    * feat(register): registere from backend
    
    * feat(login): add email verification
    
    * chore: update api url

--- app/src/main/java/ca/uqac/bigdataetmoi/authentification/SignupActivity.java
+++ app/src/main/java/ca/uqac/bigdataetmoi/authentification/SignupActivity.java
@@ -1,24 +1,26 @@
 package ca.uqac.bigdataetmoi.authentification;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 import br.com.simplepass.loading_button_lib.customViews.CircularProgressButton;
 import ca.uqac.bigdataetmoi.R;
+import ca.uqac.bigdataetmoi.data.services.HttpClient;
+import ca.uqac.bigdataetmoi.data.services.UserService;
+import ca.uqac.bigdataetmoi.dto.UserAuthenticationDto;
+import ca.uqac.bigdataetmoi.models.User;
+import ca.uqac.bigdataetmoi.utils.AuthenticationValidator;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import timber.log.Timber;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -26,6 +28,7 @@ public class SignupActivity extends AppCompatActivity {
     private CircularProgressButton btnRegister;
     private FirebaseAuth auth;
     private TextView goToLogin;
+    private final int EMAIL_ALREADY_USED_CODE = 409;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,50 +42,60 @@ public class SignupActivity extends AppCompatActivity {
         goToLogin = findViewById((R.id.register_button_login));
         btnRegister = findViewById(R.id.register_button_register);
 
-        goToLogin.setOnClickListener(new View.OnClickListener() {
+        goToLogin.setOnClickListener(view -> finish());
+    }
+
+    public void register(View view) {
+        AuthenticationValidator registerValidation = new AuthenticationValidator(fieldEmail, fieldPassword);
+
+        if (registerValidation.areLoginCredentialsValid()) {
+            sendRegisterRequest();
+        } else {
+            registerValidation.displayCredentialsErrorOnEditText();
+        }
+    }
+
+    private void sendRegisterRequest() {
+        UserAuthenticationDto userRegisterDto = new UserAuthenticationDto(fieldEmail.getText().toString(), fieldPassword.getText().toString());
+        Call<User> registerCall = new HttpClient<UserService>().create(UserService.class).register(userRegisterDto);
+        btnRegister.startAnimation();
+
+        registerCall.enqueue(new Callback<User>() {
             @Override
-            public void onClick(View view) {
-                finish();
+            public void onResponse(Call<User> call, Response<User> response) {
+                handleRegisterResponse(response);
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                handleRegisterFailure(t);
             }
         });
     }
 
-    public void register(View view) {
-        final String email = fieldEmail.getText().toString(),
-                password = fieldPassword.getText().toString();
-        final Context self = this;
+    private void revertButton() {
+        btnRegister.revertAnimation();
+        btnRegister.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.rounded_button)); // Reapply rounded shape
+    }
 
-        if (TextUtils.isEmpty(email)) {
-            fieldEmail.setError(getString(R.string.error_empty_email));
-        } else if (TextUtils.isEmpty(password)) {
-            fieldPassword.setError(getString(R.string.error_empty_password));
-        } else if (password.length() < 6) {
-            fieldPassword.setError(getString(R.string.signup_error_password_too_short));
+    private void handleRegisterResponse(Response<User> response) {
+        if (response.isSuccessful()) {
+            btnRegister.stopAnimation();
+            Toast.makeText(getApplicationContext(), "Création effectuée avec succès.", Toast.LENGTH_SHORT).show();
+            finish();
         } else {
-            btnRegister.startAnimation();
-
-            auth.createUserWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                btnRegister.stopAnimation();
-                                Toast.makeText(getApplicationContext(), "Création effectuée avec succès.", Toast.LENGTH_SHORT).show();
-                                Log.d("BDEM-SIGNUP", "createUserWithEmail:success");
-                                //redirect to login page
-                                finish();
-                            } else {
-                                btnRegister.revertAnimation();
-                                btnRegister.setBackground(ContextCompat.getDrawable(self, R.drawable.rounded_button)); // Reapply rounded shape
-
-                                if (task.getException() != null) {
-                                    Toast.makeText(getApplicationContext(), "L'adresse e-mail est déjà utilisée par un autre compte.", Toast.LENGTH_SHORT).show();
-                                } else {
-                                    Toast.makeText(getApplicationContext(), "Une erreur est survenue lors de l'authentification.", Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        }
-                    });
+            revertButton();
+            if (response.code() == EMAIL_ALREADY_USED_CODE) {
+                Toast.makeText(getApplicationContext(), "L'adresse e-mail est déjà utilisée par un autre compte.", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Une erreur est survenue lors de l'authentification.", Toast.LENGTH_SHORT).show();
+            }
         }
+    }
+
+    private void handleRegisterFailure(Throwable t) {
+        Toast.makeText(getApplicationContext(), "Erreur lors de la tentative de connexion", Toast.LENGTH_SHORT).show();
+        Timber.e(t);
+        revertButton();
     }
 }
