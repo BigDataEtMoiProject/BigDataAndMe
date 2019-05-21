4836c91152cd7584a73f97c9dc2837c313bb4bb8
IJ <isoard.jerome@gmail.com>
2019-05-21 14:25:43 +0200

    - new MainActivity created with toolbar and info Button, with a navigation drawer (menu hamburger) - Info page created

--- app/src/main/java/ca/uqac/bigdataetmoi/MainActivity.java
+++ app/src/main/java/ca/uqac/bigdataetmoi/MainActivity.java
@@ -16,8 +16,10 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -34,12 +36,11 @@ import ca.uqac.bigdataetmoi.permission_manager.PermissionActivity;
 import ca.uqac.bigdataetmoi.utils.Constants;
 import ca.uqac.bigdataetmoi.utils.Prefs;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    private Toolbar toolbar;
     private TabLayout tabLayout;
     public ViewPager viewPager;
-    private DrawerLayout drawerLayout;
+    private DrawerLayout drawer;
     private NavigationView navigationView;
 
     @Override
@@ -47,10 +48,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+//        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         viewPager = findViewById(R.id.viewpager);
         setupViewPager(viewPager);
@@ -58,10 +68,7 @@ public class MainActivity extends AppCompatActivity {
         tabLayout = findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
 
-        drawerLayout = findViewById(R.id.drawer_layout);
-
         navigationView = findViewById(R.id.nav_view);
-        setupNavListener();
 
         requestPermissions(new String[] { Manifest.permission.READ_CONTACTS,
                 Manifest.permission.ACCESS_FINE_LOCATION,
@@ -69,12 +76,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                drawerLayout.openDrawer(GravityCompat.START);
-                return true;
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_info) {
+            Intent intent = new Intent(this, InfoActivity.class);
+            startActivity(intent);
+            return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 
@@ -94,6 +115,7 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(adapter);
     }
 
+<<<<<<< master
     private void setupNavListener() {
         navigationView.setNavigationItemSelectedListener(
                 new NavigationView.OnNavigationItemSelectedListener() {
@@ -116,8 +138,74 @@ public class MainActivity extends AppCompatActivity {
                         }
                         return true;
                     }
+=======
+//    private void setupNavListener() {
+//        navigationView.setNavigationItemSelectedListener(
+//                new NavigationView.OnNavigationItemSelectedListener() {
+//                    @Override
+//                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                        drawerLayout.closeDrawers();
+//                        switch (item.getItemId()) {
+//                            case R.id.nav_profile :
+//                                if (ActivityFetcherActivity.getUserId() != null) {
+//                                    startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+//                                }
+//                                break;
+//                            case R.id.nav_parametre :
+//                                startActivity(new Intent(MainActivity.this, PermissionActivity.class));
+//                                break;
+//                            case R.id.nav_propos :
+//                                startActivity(new Intent(MainActivity.this, AboutActivity.class));
+//                                break;
+//                            case R.id.nav_deconnection :
+//                                ActivityFetcherActivity.setUserID(null);
+//                                Prefs.setBoolean(MainActivity.this, Constants.SHARED_PREFS, Constants.IS_LOGGED, false);
+//                                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+//                                overridePendingTransition(R.anim.slide_from_left, R.anim.stationary);
+//                                finish();
+//                                break;
+//                        }
+//                        return true;
+//                    }
+//                }
+//        );
+//    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.nav_accueil :
+                // Don't start MainMenuActivity if we already are in
+                // startActivity(new Intent(MainMenuActivity.this, MainMenuActivity.class));
+                break;
+            case R.id.nav_profile :
+                if (ActivityFetcherActivity.getUserId() != null) {
+                    startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+>>>>>>> - new MainActivity created with toolbar and info Button, with a navigation drawer (menu hamburger) - Info page created
                 }
-        );
+                break;
+            case R.id.nav_parametre :
+                startActivity(new Intent(MainActivity.this, PermissionActivity.class));
+                break;
+            case R.id.nav_propos :
+                startActivity(new Intent(MainActivity.this, AboutActivity.class));
+                break;
+            case R.id.nav_deconnection :
+                ActivityFetcherActivity.setUserID(null);
+                Prefs.setBoolean(this, Constants.SHARED_PREFS, Constants.IS_LOGGED, false);
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                overridePendingTransition(R.anim.slide_from_left, R.anim.stationary);
+                finish();
+                break;
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     class ViewPagerAdapter extends FragmentPagerAdapter {
