61aa8b6769d58249d51dc7f6fb3c3030c63dd052
IJ <isoard.jerome@gmail.com>
2019-05-31 13:24:01 +0200

    - explanation message fragment - reverse message list

--- app/src/main/java/ca/uqac/bigdataetmoi/ui/map/MessagesFragment.java
+++ app/src/main/java/ca/uqac/bigdataetmoi/ui/map/MessagesFragment.java
@@ -1,19 +1,24 @@
 package ca.uqac.bigdataetmoi.fragments;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -22,6 +27,7 @@ import androidx.work.ExistingPeriodicWorkPolicy;
 import androidx.work.NetworkType;
 import androidx.work.PeriodicWorkRequest;
 import androidx.work.WorkManager;
+import ca.uqac.bigdataetmoi.MainActivity;
 import ca.uqac.bigdataetmoi.R;
 import ca.uqac.bigdataetmoi.adapters.MessageAdapter;
 import ca.uqac.bigdataetmoi.data.services.UserService;
@@ -30,15 +36,19 @@ import ca.uqac.bigdataetmoi.dto.MessageDto;
 import ca.uqac.bigdataetmoi.models.Message;
 import ca.uqac.bigdataetmoi.models.User;
 import ca.uqac.bigdataetmoi.repositories.UserRepository;
-import ca.uqac.bigdataetmoi.workers.LocationWorker;
+import ca.uqac.bigdataetmoi.workers.MessageWorker;
 import ca.uqac.bigdataetmoi.workers.MessageWorker;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import timber.log.Timber;
 
+import static ca.uqac.bigdataetmoi.utils.Constants.LOCATION_PERMISSION_RESULT_CODE;
+
+
 public class MessagesFragment extends Fragment {
 
+    public static final int REQUEST_CODE_SMS_PERMISSIONS = 123;
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
@@ -56,25 +66,36 @@ public class MessagesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        startMessageBackgroundWork();
 
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_messages, container, false);
+//        return inflater.inflate(R.layout.fragment_message_permission, container, false);
+
+
+        if (hasAlreadyAcceptedMessagePermission()) {
+            startMessageBackgroundWork();
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.fragment_messages, container, false);
+        } else {
+            return inflater.inflate(R.layout.fragment_message_permission, container, false);
+        }
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        recyclerView = (RecyclerView) getView().findViewById(R.id.my_recycler_view);
+        addMessagePermissionButtonListener();
 
-        // use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
-        recyclerView.setHasFixedSize(true);
+        if (hasAlreadyAcceptedMessagePermission()) {
+            recyclerView = (RecyclerView) getView().findViewById(R.id.my_recycler_view);
 
-        // use a linear layout manager
-        layoutManager = new LinearLayoutManager(getContext());
-        recyclerView.setLayoutManager(layoutManager);
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            recyclerView.setHasFixedSize(true);
+
+            // use a linear layout manager
+            layoutManager = new LinearLayoutManager(getContext());
+            recyclerView.setLayoutManager(layoutManager);
+        }
         messages = new ArrayList<>();
     }
 
@@ -97,6 +118,18 @@ public class MessagesFragment extends Fragment {
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE_SMS_PERMISSIONS) {
+            if (hasGrantedLocationPermission(grantResults)) {
+                // Display messages
+                if (getActivity() != null) {
+                    ((MainActivity) getActivity()).refreshViewPager();
+                }
+            }
+        }
+    }
+
+    @Override
     public void onAttach(Context context) {
         super.onAttach(context);
 
@@ -106,6 +139,24 @@ public class MessagesFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
 
+    }
+
+    private void addMessagePermissionButtonListener() {
+        Button MessagePermissionButton = getView().findViewById(R.id.message_continue);
+
+        if (MessagePermissionButton != null) {
+            MessagePermissionButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    askForMessagePermission();
+                }
+            });
+        }
+    }
+
+    private void askForMessagePermission() {
+        requestPermissions(new String[]{Manifest.permission.READ_SMS},
+                REQUEST_CODE_SMS_PERMISSIONS);
     }
 
     private void startMessageBackgroundWork() {
@@ -132,8 +183,6 @@ public class MessagesFragment extends Fragment {
 
             if (user != null) {
                 updateMessages(user);
-//                updateRecentMoves(user);
-//                updateLastUpdateTime(user);
                 Timber.d("§ handleUserResponse");
             }
         } else {
@@ -141,12 +190,13 @@ public class MessagesFragment extends Fragment {
         }
     }
 
-    public void updateMessages(User user){
-        for(int i=0; i<user.messageList.size(); i++){
+    public void updateMessages(User user) {
+        for (int i = 0; i < user.messageList.size(); i++) {
             Message message = user.messageList.get(i);
             messages.add(message);
         }
-        if(messages.size() > 0) {
+        Collections.reverse(messages);
+        if (messages.size() > 0) {
             messages.add(0, new Message("header", messages.get(0).date, ""));
             for (int i = 1; i < messages.size(); i++) {
                 // if the message date changes, insert a header message in the arraylist
@@ -156,7 +206,18 @@ public class MessagesFragment extends Fragment {
             }
         }
 
-        mAdapter = new MessageAdapter(messages, getContext());
-        recyclerView.setAdapter(mAdapter);
+        if (hasAlreadyAcceptedMessagePermission()) {
+            mAdapter = new MessageAdapter(messages, getContext());
+            recyclerView.setAdapter(mAdapter);
+        }
+    }
+
+    private boolean hasAlreadyAcceptedMessagePermission() {
+        return (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_SMS)
+                == PackageManager.PERMISSION_GRANTED);
+    }
+
+    private boolean hasGrantedLocationPermission(int[] grantResults) {
+        return grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED;
     }
 }
