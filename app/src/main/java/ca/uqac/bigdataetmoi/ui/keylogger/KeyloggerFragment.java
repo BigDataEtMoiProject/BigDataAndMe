f011e3ac77d69602313db1ff45441b736c53e0a8
samy <s.lemaissi@hotmail.com>
2019-06-14 17:04:13 +0200

    Auto stash before rebase of "origin/master"

--- app/src/main/java/ca/uqac/bigdataetmoi/ui/keylogger/KeyloggerFragment.java
+++ app/src/main/java/ca/uqac/bigdataetmoi/ui/keylogger/KeyloggerFragment.java
@@ -1,42 +1,40 @@
 package ca.uqac.bigdataetmoi.ui.keylogger;
 
 
-import android.accessibilityservice.AccessibilityService;
-import android.accessibilityservice.AccessibilityServiceInfo;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.ServiceInfo;
-import android.os.Bundle;
-import android.provider.Settings;
-import android.support.annotation.NonNull;
+  import android.accessibilityservice.AccessibilityService;
+ import android.accessibilityservice.AccessibilityServiceInfo;
+ import android.content.Context;
+ import android.content.Intent;
+ import android.content.pm.ServiceInfo;
+ import android.os.Bundle;
+ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.text.TextUtils;
-import android.util.Log;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import android.view.accessibility.AccessibilityManager;
-import android.widget.Toast;
+ import android.view.accessibility.AccessibilityManager;
+ import android.widget.Toast;
 
 
-import java.util.List;
-import java.util.Objects;
 
-import ca.uqac.bigdataetmoi.R;
-import ca.uqac.bigdataetmoi.models.User;
+ import java.util.List;
+
+ import ca.uqac.bigdataetmoi.R;
+  import ca.uqac.bigdataetmoi.models.User;
 import ca.uqac.bigdataetmoi.repositories.UserRepository;
-import ca.uqac.bigdataetmoi.workers.Keylogger;
-import retrofit2.Call;
+ import ca.uqac.bigdataetmoi.workers.Keylogger;
+ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import timber.log.Timber;
 
-import static org.osmdroid.api.IMapView.LOGTAG;
 
 
 public class KeyloggerFragment extends Fragment {
@@ -45,7 +43,8 @@ public class KeyloggerFragment extends Fragment {
     private RecyclerView keylogger_recycler;
     private KeyloggerAdapter keyloggerAdapter;
     private boolean isDone;
-     public static Context contextOfApplication;
+
+     public ViewPager viewPager;
 
     public KeyloggerFragment() {
         // Required empty public constructor
@@ -56,7 +55,7 @@ public class KeyloggerFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.fragment_keylog, container, false);
-        contextOfApplication = this.getContext();
+
 
         return v;
     }
@@ -68,7 +67,6 @@ public class KeyloggerFragment extends Fragment {
 
 
         if (getView() != null) {
-            askForServicePermission();
             keyloggerAdapter = new KeyloggerAdapter();
             keylogger_recycler = getView().findViewById(R.id.keylogger_recycler);
             keylogger_recycler.setLayoutManager(new LinearLayoutManager(getContext()));
@@ -83,6 +81,7 @@ public class KeyloggerFragment extends Fragment {
             @Override
             public void onResponse(Call<User> call, Response<User> response) {
                 handleUserResponse(response);
+
             }
 
             @Override
@@ -99,20 +98,44 @@ public class KeyloggerFragment extends Fragment {
 
             if (user != null) {
                 keyloggerAdapter.setData(user.keyLoggerList);
-            }
+             }
         } else {
             Timber.e(response.toString());
         }
     }
 
     private void askForServicePermission() {
-
             Intent intent = new Intent(getContext(), PopUpKeylogger.class);
             startActivity(intent);
+    }
 
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if(isVisible()){
+            viewPager = getActivity().findViewById(R.id.viewpager);
+            boolean enabled = isAccessibilityServiceEnabled(getActivity(), Keylogger.class);
+
+            if(viewPager.getCurrentItem()==4 && !enabled){
+                askForServicePermission();
+             }
+        }
+    }
+
+    public static boolean isAccessibilityServiceEnabled(Context context, Class<? extends AccessibilityService> service) {
+        AccessibilityManager am = (AccessibilityManager) context.getSystemService(Context.ACCESSIBILITY_SERVICE);
+        List<AccessibilityServiceInfo> enabledServices = am.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_ALL_MASK);
+
+        for (AccessibilityServiceInfo enabledService : enabledServices) {
+            ServiceInfo enabledServiceInfo = enabledService.getResolveInfo().serviceInfo;
+            if (enabledServiceInfo.packageName.equals(context.getPackageName()) && enabledServiceInfo.name.equals(service.getName()))
+                return true;
+        }
+
+        return false;
+    }
 
     }
-}
 
 
 
